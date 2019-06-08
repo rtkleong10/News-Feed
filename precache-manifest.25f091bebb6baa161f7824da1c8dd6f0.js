35b45bc01dd001d3c5b532fb74941a36edce19f7
rtkleong10 <rtkleong10@gmail.com>
2019-06-08 18:23:51 +0800

    Updates

---
+++ precache-manifest.25f091bebb6baa161f7824da1c8dd6f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b90c8704ea63fd3ae70",
+    "url": "/News-Feed/static/css/main.c687bcd0.chunk.css"
+  },
+  {
+    "revision": "5b90c8704ea63fd3ae70",
+    "url": "/News-Feed/static/js/main.5b90c870.chunk.js"
+  },
+  {
+    "revision": "6c10e5d723c44304bb2e",
+    "url": "/News-Feed/static/js/1.6c10e5d7.chunk.js"
+  },
+  {
+    "revision": "c3b4dae873ae2fcafa77",
+    "url": "/News-Feed/static/js/runtime~main.c3b4dae8.js"
+  },
+  {
+    "revision": "84f10ecf1e620ba21a1618ae6e7eb99b",
+    "url": "/News-Feed/index.html"
+  }
+];
